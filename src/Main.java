95e3348593c45c6a486d3c9a266ce5ebe64bc36e
Michael Kaiser <michael.kaiser@bearingpoint.com>
2018-05-01 14:51:51 +0200

    master 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! - master 1");
+        System.out.println("Hello World! - master 2");
     }
 }
