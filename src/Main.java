fd9d9d529dcc778fc13860826ce5c42177d4bbde
Michael Kaiser <michael.kaiser@bearingpoint.com>
2018-05-01 20:00:54 +0200

    feature 2.2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! - master 1.1");
-        System.out.println("Hello World! - feature 2.1");
+        System.out.println("Hello World! - feature 2.2");
     }
 }
