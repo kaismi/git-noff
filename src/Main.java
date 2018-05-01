239ff50a84cad472017118d600471cae4f25966f
Michael Kaiser <michael.kaiser@bearingpoint.com>
2018-05-01 20:03:35 +0200

    master 1.4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! - master 1.3");
+        System.out.println("Hello World! - master 1.4");
     }
 }
