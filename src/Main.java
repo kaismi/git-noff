8fd397b998cd50303cab8c28f877608cc28e2922
Michael Kaiser <michael.kaiser@bearingpoint.com>
2018-05-01 19:43:29 +0200

    feature 4

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! - master 2.1");
         System.out.println("Hello World! - feature 3.1");
+        System.out.println("Hello World! - feature 4");
     }
 }
