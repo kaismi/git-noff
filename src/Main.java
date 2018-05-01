dd97c356cb73cb295bd1e81cc7e2ebcb909d7644
Michael Kaiser <michael.kaiser@bearingpoint.com>
2018-05-01 14:49:50 +0200

    master init

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! - master 2.1");
-        System.out.println("Hello World! - feature 1");
-        System.out.println("Hello World! - feature 2");
+        System.out.println("Hello World! - master");
     }
 }
