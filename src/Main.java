2c069287b6afe4bfa200238715ef6607efd76235
Michael Kaiser <michael.kaiser@bearingpoint.com>
2018-05-09 07:25:47 +0200

    master 1.10

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! - master 1.9");
+        System.out.println("Hello World! - master 1.10");
         System.out.println("Hello World! - feature 2.3");
         System.out.println("Hello World! - feature 1.2");
         System.out.println("Hello World! - feature 3.0");
